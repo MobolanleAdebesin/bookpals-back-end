6c37af2620b9a970fc5197469ddf52a59635ba26
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-11-13 10:33:58 -0500

    Created home route

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const app = express();
-const Book = require("./models/Books.js");
+const Book = require("./lib/models/Books.js");
+const parser = require("body-parser");
+app.use(parser.json());
+
+app.get("/", function(req, res) {
+  Book.find({}).then(books => {
+    res.json(books);
+  });
+});
+
+app.listen(4000, function() {
+  console.log("I am listening at port 4000");
+});
